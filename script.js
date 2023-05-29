3e0a2295a6fa6c666e22bde82be1048937a6d5df
VENGATESH.S <venkatesh11315@outlook.com>
2023-05-29 13:13:25 +0530

    call back

---
+++ script.js
@@ -0,0 +1,47 @@
+setTimeout(function () {
+    document.getElementById("count").innerHTML = 10;
+
+    setTimeout(function () {
+        document.getElementById("count").innerHTML = 9;
+
+        setTimeout(function () {
+            document.getElementById("count").innerHTML = 8;
+
+            setTimeout(function () {
+                document.getElementById("count").innerHTML = 7;
+                setTimeout(function () {
+                    document.getElementById("count").innerHTML = 6;
+
+                    setTimeout(function () {
+                        document.getElementById("count").innerHTML = 5;
+                        setTimeout(function () {
+                            document.getElementById("count").innerHTML = 4;
+
+                            setTimeout(function () {
+                                document.getElementById("count").innerHTML = 3;
+                                setTimeout(function () {
+                                    document.getElementById("count").innerHTML = 2;
+                                    setTimeout(function () {
+                                        document.getElementById("count").innerHTML = 1;
+                                        setTimeout(function () {
+                                            document.getElementById("count").innerHTML = "HAPPY INDEPENDENCE DAY";
+
+                                        }, 2000)
+                                    }, 2000)
+
+                                }, 2000)
+
+                            }, 2000)
+                        }, 2000)
+
+                    }, 2000)
+                }, 2000)
+
+
+            }, 2000)
+        }, 2000)
+
+    }, 2000)
+
+
+}, 1000)
